b5c52a4f6537ae3944c53e58f223fb31a00e9a59
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2021-01-21 22:21:46 +0530

    Create App.js

---
+++ To-Do-App/src/App.js
@@ -0,0 +1,100 @@
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
+
+//Let's write a class based compnent
+//Let's render. That means we will return some stuff
+class App extends React.Component {
+
+
+constructor(props){
+  super(props);
+  this.state={
+    newItem: "",
+    list:[]
+  }
+}
+
+addItem(todoValue){
+  if(todoValue !=="")
+  {
+    const newItem ={
+      id: Date.now(),
+      value: todoValue,
+      isDone: false
+    };
+    const list = [...this.state.list];
+    list.push(newItem);
+
+    this.setState({
+      list, 
+      newItem: ""
+    });
+  }
+}
+
+
+//Creating another method to delete the Items or rather making the delete button in workig state.
+deleteItem(id){
+  const list =[...this.state.list];
+  const updatedlist = list.filter(item =>item.id !== id);
+  this.setState({list: updatedlist})
+}
+
+updateInput(input){
+  this.setState({newItem:input});
+}
+
+
+
+ render()
+ {
+    return(
+      <div>
+        
+        <img src={logo} width="100" height="100"  className="logo" />
+        <h1 className="app-title">TO DO APP </h1>
+        <div className="container">Add an Item...<br/>
+        <input
+        type="text"
+        className="input-text" 
+        placeholder="Write a to do" 
+        required
+        value={this.state.newItem}
+        onChange={e => this.updateInput(e.target.value)}
+        />
+        <button
+         className="add-btn"
+         onClick={() => this.addItem(this.state.newItem)}
+         disabled={!this.state.newItem.length}
+         >Add To Do</button>
+        <div className="list">
+          <ul>
+            {this.state.list.map(item =>{
+              return(
+                <li key={item.id}>
+                  <input type="checkbox" name="idDone" checked={item.isDone} onChange={()=>{}}/>
+                  {item.value}
+                  <button
+                  className="btn"
+                  onClick={()=>this.deleteItem(item.id)}
+                  >Delete</button>
+                </li>
+              );
+            })}
+            <li>
+              <input type="checkbox" name="" id="" />
+              Record Youtube Videos
+              <button className="btn">DELETE</button>
+            </li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    );
+ }
+  }
+
+
+  //Let's export what we created
+  export default App;
